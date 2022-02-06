682be814561d89a1d11d4ba0c0a406f60ec51f74
yomZsamora <samora.y@adzumi.co.ke>
2022-02-06 13:43:52 +0300

    Completed Lab.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Your code goes here
+// Changes the text on the page to "This is really cool!" when DOM content has loaded
 document.addEventListener("DOMContentLoaded", function() {
 	document.getElementById('text').innerHTML = "This is really cool!";
 });
